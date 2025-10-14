70780c376accd986e41fc5f80f9a355540f2b59f
MeowExort <tobilts@yandex.ru>
2025-10-14 15:30:59 +0300

    init

---
+++ Pw.Hub/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Pw.Hub;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
